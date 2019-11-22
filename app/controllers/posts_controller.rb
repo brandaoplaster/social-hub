ce8f9dea73696f65b61ad53f60b73eb54e51088f
brandaoplaster <brandaoplaster@gmail.com>
2019-11-22 15:05:23 -0300

    Implemented controller post index method to return user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+  before_action :set_post, only: [:show, :edit, update, :destroy]
+
   def index
+    @user = current_user
+    @post = current_user.posts.map { |post| post }
+    current_user.all_following.each { |user| user.posts.each { |post| post << post } }
+    @posts = posts.sort_by &:created_at
   end
 
   def create
