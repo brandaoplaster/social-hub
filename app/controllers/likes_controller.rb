46bf1d9901a0fff3c035553b7614f3baf00ea19e
brandaoplaster <brandaoplaster@gmail.com>
2019-11-22 17:05:37 -0300

    Created controller likes create method to add a new like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,20 @@
 class LikesController < ApplicationController
   before_action :set_post
   before_action :set_event, only: [:destroy]
-  laod_and_authorize_resource
+  load_and_authorize_resource
 
   def create
+    @like = Like.new(user: current_user, post: @post)
+
+    respond_to do |format|
+      if @like.save
+        format.json { render json: @like }
+        format.html { redirect_to :back }
+      else
+        format.json { render json: @like.errors, status: :unprocessable_entity }
+        format.html { redirect_to :back }
+      end
+    end
   end
 
   def destroy
