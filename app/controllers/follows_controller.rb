577ff631988c5fb74a9ee96be31edd359437468a
brandaoplaster <brandaoplaster@gmail.com>
2019-11-24 00:27:37 -0300

    Created method for user to follow other users

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,7 +2,15 @@ class FollowsController < ApplicationController
   before_action :set_user
 
   def create
-
+    respond_to do |format|
+      if current_user.follow(@user)
+        format.json { render @user, status: :created }
+        format.html { redirect_to :back, notice: 'User follows with success' }
+      else
+        format.json { render json: nil, status: :unprocessable_entity }
+        format.html { redirect_to :back }
+      end
+    end
   end
 
   def destroy
