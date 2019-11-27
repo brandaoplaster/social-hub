ed5c288f69f42e83a23f9786b191df66746cab27
brandaoplaster <brandaoplaster@gmail.com>
2019-11-26 21:46:03 -0300

    Creating a search method is included before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_search
 
   protected
 
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(current_user)
     posts_path
+  end
+
+  def set_search
+    @q = User.ransack(params[:q])
   end
 end
